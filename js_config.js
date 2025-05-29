d1059275f585858fe36456307dd876d034cbf522
Tanansh-Ahuja <tananshahuja17@gmail.com>
2025-05-29 18:04:22 +0530

    JS CONFIG API BASE URL FIX

--- js_config.js
+++ js_config.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = 'http://localhost:3000'; // change this only here when moving to Railway
-export const BASE_URL = "https://api.skywardweb.in"; //production
+export const API_BASE_URL = "https://api.skywardweb.in"; //production
